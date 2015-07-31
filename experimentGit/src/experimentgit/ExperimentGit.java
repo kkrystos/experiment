e366f95572cedffd8ca723f82df1a98d5f7fb946
kkrystos <kkrystos@gmail.com>
2015-07-31 15:50:37 +0200

    System.out.println("try revert from netbans");

--- experimentGit/src/experimentgit/ExperimentGit.java
+++ experimentGit/src/experimentgit/ExperimentGit.java
@@ -7,8 +7,7 @@ package experimentgit;
 public class ExperimentGit {
 
     public static void main(String[] args) {
-        System.out.println("changed in cmd");
-        System.out.println("changed in cmd");
+        System.out.println("try revert from netbans");
     }
 
 }
--- experimentGit/src/experimentgit/SecondClass.java
+++ experimentGit/src/experimentgit/SecondClass.java
@@ -7,8 +7,7 @@ package experimentgit;
 public class SecondClass {
 
     public void test() {
-        System.out.println("changed in cmd");
-        System.out.println("changed in cmd");
+        System.out.println("try revert from netbans");
     }
 
 }
